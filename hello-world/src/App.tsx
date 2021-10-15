5b8377318df087949e5063b3d9e4f51c2040fb8d
Renan Rodrigues <renan_ime_2012@hotmail.com>
2021-10-15 00:39:15 -0300

    renderização condicional

--- hello-world/src/App.tsx
+++ hello-world/src/App.tsx
@@ -4,21 +4,32 @@ import { Footer } from './components/Footer'
 
 const App = () => {
 
-    let name = "Renan";
-    let age = 90;
-
+    const [name, setName] = useState("Renan");
+    const [age, setAge] = useState(90);
     const [bg, setBg] = useState("#FF0000")
     
-    const handleClick = () => {
+    const handleClick20 = () => {
+        setAge(20)
         setBg('#0000FF')
+    }
+
+    const handleClick90 = () => {
+        setAge(90)
+        setBg('#FF0000')
     }
 
     return (
         <div style={{backgroundColor: bg}}>
         <Header name={name} age={age}/>
         
-        <button onClick={handleClick}>Click aqui</button>
-        
+
+        {age === 90 && 
+        <button onClick={handleClick20}>Click aqui</button>
+        }
+        {age === 20 &&
+        <button onClick={handleClick90}>Click aqui</button>
+        }
+
         <Footer/>
         </div>
     )
